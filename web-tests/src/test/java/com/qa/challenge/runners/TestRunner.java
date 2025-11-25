3d44a1aa2a49b15e9cdafb9d6689757aea71bf0b
vbtony6 <vbtony6@gmail.com>
2025-11-25 01:10:54 -0500

    Fix Cucumber glue configuration

--- web-tests/src/test/java/com/qa/challenge/runners/TestRunner.java
+++ web-tests/src/test/java/com/qa/challenge/runners/TestRunner.java
@@ -1,19 +1,7 @@
 package com.qa.challenge.runners;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-
-@Suite
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.qa.challenge.steps")
-
-// Plugins compatibles con JUnit 5
-@ConfigurationParameter(
-        key = PLUGIN_PROPERTY_NAME,
-        value = "pretty, summary, json:target/cucumber-report.json"
-)
-public class TestRunner { }
+@Cucumber
+public class TestRunner {
+}
